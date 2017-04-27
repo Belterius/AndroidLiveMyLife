c8aba71ac5db26298f3d58753db77036d1b83757
Francois Dieval <warlife62@gmail.com>
2017-04-27 17:49:05 +0200

    [FDL] Création du menu principal et suppression de la MainActivity

--- app/src/main/java/lml/androidlivemylife/LocalStoriesActivity.java
+++ app/src/main/java/lml/androidlivemylife/LocalStoriesActivity.java
@@ -3,24 +3,65 @@ package lml.androidlivemylife;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class LocalStoriesActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_new:
+                    mTextMessage.setText(R.string.title_new_story);
+                    return true;
+                case R.id.navigation_browse:
+                    mTextMessage.setText(R.string.title_browse_story);
+                    return true;
+                case R.id.navigation_account:
+                    mTextMessage.setText(R.string.title_my_account);
+                    return true;
+            }
+            return false;
+        }
+
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_local_activities);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
         checkLocationPermission();
     }
 
+
     public void goToMaps(View v){
-        startActivity(new Intent(MainActivity.this, MapsActivity.class));
+        startActivity(new Intent(this, MapsActivity.class));
     }
+
+    public void goToLocalStories(View v){
+        startActivity(new Intent(this, LocalStoriesActivity.class));
+    }
+
     public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
     public boolean checkLocationPermission(){
         if (ContextCompat.checkSelfPermission(this,

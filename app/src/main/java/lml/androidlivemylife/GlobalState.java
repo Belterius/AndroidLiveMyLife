ed1e85c8c119cfa283dfea82101b169da848afd1
gimlibeta <gimlibeta@gmail.com>
2017-04-27 16:47:20 +0200

    Create GeneralClass

--- app/src/main/java/lml/androidlivemylife/GlobalState.java
+++ app/src/main/java/lml/androidlivemylife/GlobalState.java
@@ -8,7 +8,7 @@ import android.widget.Toast;
  * Created by Gimlibéta on 27/04/2017.
  */
 
-public class GeneralClass extends Application{
+public class GlobalState extends Application{
 
     void alerter(String s){
         Log.i("LiveMyLife", s);

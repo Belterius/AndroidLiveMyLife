ad3fc303151f47a11155768d0e6abe48175d7988
gimlibeta <gimlibeta@gmail.com>
2017-04-27 17:02:57 +0200

    Global State - Reprise du code du tp2 chat

--- app/src/main/java/lml/androidlivemylife/GlobalState.java
+++ app/src/main/java/lml/androidlivemylife/GlobalState.java
@@ -1,19 +1,127 @@
 package lml.androidlivemylife;
 
 import android.app.Application;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.util.Log;
 import android.widget.Toast;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.CookieHandler;
+import java.net.CookieManager;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
- * Created by Gimlibéta on 27/04/2017.
+ * Created by Gimlibéta on 21/02/2017.
  */
 
 public class GlobalState extends Application{
 
+    public String CAT = "LiveMyLife";
+    public CookieManager cookieManager;
+
+    public Boolean connected = false;
+    /*public String login;
+    public String pass;*/
+    public int notificationId = 1;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        cookieManager = new CookieManager();
+        CookieHandler.setDefault(cookieManager);
+    }
+
     void alerter(String s){
-        Log.i("LiveMyLife", s);
+        Log.i(CAT, s);
         Toast t = Toast.makeText(this, s, Toast.LENGTH_LONG);
         t.show();
+    }
+
+
+    private String convertStreamToString(InputStream in) throws IOException {
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            return sb.toString();
+        }finally {
+            try {
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    public boolean verifReseau()
+    {
+        // On vérifie si le réseau est disponible,
+        // si oui on change le statut du bouton de connexion
+        ConnectivityManager cnMngr = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cnMngr.getActiveNetworkInfo();
+
+        String sType = "Aucun réseau détecté";
+        Boolean bStatut = false;
+        if (netInfo != null)
+        {
+
+            NetworkInfo.State netState = netInfo.getState();
+
+            if (netState.compareTo(NetworkInfo.State.CONNECTED) == 0)
+            {
+                bStatut = true;
+                int netType= netInfo.getType();
+                switch (netType)
+                {
+                    case ConnectivityManager.TYPE_MOBILE :
+                        sType = "Réseau mobile détecté"; break;
+                    case ConnectivityManager.TYPE_WIFI :
+                        sType = "Réseau wifi détecté"; break;
+                }
+
+            }
+        }
+
+        this.alerter(sType);
+        return bStatut;
+    }
+
+    public String requete(String qs) {
+        if (qs != null)
+        {
+            String urlData = "https://api.livemylife.coniface.fr/verif.php";
+
+            try {
+                URL url = new URL(urlData + "?" + qs);
+                Log.i(CAT,"url utilisée : " + url.toString());
+                HttpURLConnection urlConnection = null;
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = null;
+                in = new BufferedInputStream(urlConnection.getInputStream());
+                String txtReponse = convertStreamToString(in);
+                urlConnection.disconnect();
+                return txtReponse;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        return "";
     }
 
 }

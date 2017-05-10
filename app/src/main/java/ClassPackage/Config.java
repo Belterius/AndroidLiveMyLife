061dee00e81b90246678b86e6219039ca5f0edfe
gimlibeta <gimlibeta@gmail.com>
2017-05-10 14:02:37 +0200

    [temp] Premieres approches pour la gestion d'images (non finalisée)

--- app/src/main/java/ClassPackage/Config.java
+++ app/src/main/java/ClassPackage/Config.java
@@ -5,4 +5,11 @@ package ClassPackage;
  */
 
 public class Config {
+
+    // File upload url (replace the ip with your server address)
+    public static final String FILE_UPLOAD_URL = "http://localhost/PHP-LiveMyLife/libs/upload.php";
+
+    // Directory name to store captured images and videos
+    public static final String IMAGE_DIRECTORY_NAME = "PHP-LiveMyLife/uploads/";
+
 }

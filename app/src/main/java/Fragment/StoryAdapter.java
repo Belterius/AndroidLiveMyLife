b06fdb28cc75acc2d5a8d0f6214ba05efc4b452a
Francois Dieval <warlife62@gmail.com>
2017-05-28 23:31:57 +0200

    [feat] Gestion du clic sur les boutons de story

--- app/src/main/java/Fragment/StoryAdapter.java
+++ app/src/main/java/Fragment/StoryAdapter.java
@@ -9,9 +9,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -58,6 +60,9 @@ public class StoryAdapter extends BaseAdapter {
         TextView title;
         TextView desc;
         ImageView img;
+        ImageButton imgb1;
+        ImageButton imgb2;
+        ImageButton imgb3;
     }
     @Override
     public View getView(final int position, View convertView, ViewGroup parent) {
@@ -65,20 +70,37 @@ public class StoryAdapter extends BaseAdapter {
         Holder holder=new Holder();
         View rowView;
         rowView = inflater.inflate(R.layout.listview_item_row, null);
+
         holder.title=(TextView) rowView.findViewById(R.id.txtTitle);
         holder.desc=(TextView) rowView.findViewById(R.id.txtDesc);
         holder.img=(ImageView) rowView.findViewById(R.id.imgIcon);
+        holder.imgb1= (ImageButton) rowView.findViewById(R.id.storybutton1);
+        holder.imgb2= (ImageButton) rowView.findViewById(R.id.storybutton2);
+        holder.imgb3= (ImageButton) rowView.findViewById(R.id.storybutton3);
+
         holder.title.setText(data.get(position).getTitle());
         holder.desc.setText(data.get(position).getDescription());
         holder.img.setImageResource(R.drawable.magnifier);
         //holder.img.setImageResource(data.get(position).getHighlight());
-        /*rowView.setOnClickListener(new OnClickListener() {
+        holder.imgb1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO Auto-generated method stub
-                Toast.makeText(context, "You Clicked "+result[position], Toast.LENGTH_LONG).show();
+                Toast.makeText(parent.getContext(), "Publish " + holder.title.getText(), Toast.LENGTH_LONG).show();
             }
-        });*/
+        });
+        holder.imgb2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(parent.getContext(), "Edit " + holder.title.getText(), Toast.LENGTH_LONG).show();
+            }
+        });
+        holder.imgb3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(parent.getContext(), "Delete " + holder.title.getText(), Toast.LENGTH_LONG).show();
+            }
+        });
+
         return rowView;
     }
 }

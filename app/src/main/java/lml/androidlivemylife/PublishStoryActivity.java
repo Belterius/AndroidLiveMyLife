5cb14dd50d07226067b1d32083d343a775866272
Francois Dieval <warlife62@gmail.com>
2017-05-30 11:34:15 +0200

    [feat] Publish story fonctionnel (+écran de partage avec boutons non fonctionnels)

---
+++ app/src/main/java/lml/androidlivemylife/PublishStoryActivity.java
@@ -0,0 +1,64 @@
+package lml.androidlivemylife;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import API_request.MySingleton;
+import ClassPackage.GlobalState;
+import ClassPackage.MyUser;
+
+public class PublishStoryActivity extends AppCompatActivity {
+
+    final public String TAG = "publishStory";
+    private TextView title;
+    private ImageButton previousarrow;
+    private GlobalState gs;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_publish_story);
+
+        title = (TextView) findViewById(R.id.titleStory);
+        previousarrow = (ImageButton) findViewById(R.id.publish_story_previous_button);
+
+        Bundle b = this.getIntent().getExtras();
+        String s = b.getString("storyTitle").toString();
+        this.title.setText(s);
+
+        previousarrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goBackToLocalStories();
+            }
+        });
+
+        gs = new GlobalState();
+    }
+
+    @Override
+    protected void onStop () {
+        super.onStop();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    private void goBackToLocalStories(){
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        finish();
+    }
+
+}

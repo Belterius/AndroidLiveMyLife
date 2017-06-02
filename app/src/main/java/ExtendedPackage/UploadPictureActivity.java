bbc5873d5b845d1f4d3beec3bd979e0db933d168
gimlibeta <gimlibeta@gmail.com>
2017-06-02 14:54:37 +0200

    [fix] Compressage à 90% (prendre moins de place sur le serveur de victor)

--- app/src/main/java/ExtendedPackage/UploadPictureActivity.java
+++ app/src/main/java/ExtendedPackage/UploadPictureActivity.java
@@ -220,7 +220,7 @@ public class UploadPictureActivity extends AppCompatActivity {
      */
     public String getStringImage(Bitmap bmp){
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        bmp.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        bmp.compress(Bitmap.CompressFormat.JPEG, 10, baos);
         byte[] imageBytes = baos.toByteArray();
         String encodedImage = Base64.encodeToString(imageBytes, Base64.DEFAULT);
 

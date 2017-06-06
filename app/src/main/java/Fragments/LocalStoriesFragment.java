3726ee2f02f25697925b550aad87dabda445e331
gimlibeta <gimlibeta@gmail.com>
2017-06-06 19:13:13 +0200

    [fix] String statique en dynamique

--- app/src/main/java/Fragments/LocalStoriesFragment.java
+++ app/src/main/java/Fragments/LocalStoriesFragment.java
@@ -193,7 +193,7 @@ public class LocalStoriesFragment extends Fragment {
             }
 
         } catch (JSONException e) {
-            this.title_local_stories.setText("No stories created yet. Start sharing !");
+            this.title_local_stories.setText(R.string.error_no_stories);
             e.printStackTrace();
         }
 
